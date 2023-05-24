1dff13ce3a0a0446639f76ad538695cbc4cad72c
Lameck Meshack <rhinolamer@gmail.com>
2023-05-24 07:10:53 +0300

    rename frontend to client

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <header>TS amazone</header>
+        <footer>footer</footer>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
