5b75a30ab157b5e7d01a4fa636e5a274bb8b1162
Lameck Meshack <rhinolamer@gmail.com>
2023-05-27 06:07:09 +0300

    Fetching single product using react query

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,6 +15,16 @@ app.get("/api/products", (req: Request, res: Response) => {
     res.json(sampleProducts);
 });
 
+app.get("/api/products/:slug", (req: Request, res: Response) => {
+    const product = sampleProducts.find((x) => x.slug === req.params.slug);
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).json({ message: "Product not found" });
+    }
+});
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
